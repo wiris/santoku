20ddce67457d541508969a6b6d068607a324e3fa
didac_fernandez_cadenas <didac@wiris.com>
2019-11-15 19:04:34 +0100

    Add more functionality + PEP8 linting
    Apply a PEP8 linter (camelcase on object names, double quotes for
    docstrings)
    Add several methods for handling objects in S3Tools
    Add an untested method for iterating an S3 prefix within a bucket
    Create .gitignore with pycharm metadata
    Change README accordingly

--- README.md
+++ README.md
@@ -27,13 +27,13 @@ instance, this is an example to read files from a S3 bucket folder.
 ```
 from wiris.aws import s3tools
 
-s3tools = s3tools()
+s3tools = S3Tools()
 
 s3_bucket = 'wiris-bucket'
 s3_prefix = 'input_folder'
 for file_key in s3tools.get_keys_as_generator(s3_bucket, s3_prefix):
     print('Read file "{}"'.format(file_key))
     file_content = s3tools.get_file_content(s3_bucket, file_key)
-    
+
     # do something with content    
 ```
--- src/wiris/aws/s3tools.py
+++ src/wiris/aws/s3tools.py
@@ -1,20 +1,21 @@
 import boto3
 from io import StringIO
 
-class s3tools:
-    '''
+
+class S3Tools:
+    """
     Class to handle input/output operations with Amazon's S3 storage service.
 
     This class is intended to be run on AWS Glue job (Python Shell).
-    '''
+    """
 
     def __init__(self):
         self.client = boto3.client('s3')
         self.resource = boto3.resource('s3')
 
     # Function to get all keys (files) from S3 bucket and prefix
-    def get_keys_as_generator(self, bucket, prefix):
-        kwargs = {'Bucket': bucket, 'Prefix': prefix}
+    def get_keys_as_generator(self, bucket, prefix, start_after='null'):
+        kwargs = {'Bucket': bucket, 'Prefix': prefix, 'StartAfter': start_after}
         while True:
             resp = self.client.list_objects_v2(**kwargs)
             for obj in resp['Contents']:
@@ -25,6 +26,23 @@ class s3tools:
             except KeyError:
                 break
 
+    '''
+    def paginate(self, method, **kwargs):
+    """
+    Same as get_keys_as_generator but not limited to 1k objects, generic syntax for other services other than s3 and
+    other methods other than list_objects_v2. Needs testing
+    :param method: method to list the objects, here it will usually be self.client.list_objects_v2
+    :param kwargs: arguments for the above method, e.g.
+    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.list_objects_v2
+    in this case, for list_objects_v2 kwargs would need Bucket, Prefix and possibly StartAfter
+    :return: yields an iterable with the objects in the s3
+    """
+    paginator = self.client.get_paginator(method.__name__)
+    for page in paginator.paginate(**kwargs).result_key_iters():
+        for result in page:
+            yield result
+    '''
+
     # Read file at s3 bucket with given key. Use provided encoding
     def get_file_content(self, bucket, file_key, encoding='utf-8'):
         file_obj = self.resource.Object(bucket, file_key)
@@ -34,6 +52,21 @@ class s3tools:
     def write_file(self, content, bucket, file_key):
         self.client.put_object(Body=content, Bucket=bucket, Key=file_key)
 
+    def delete_file(self, bucket, file_key):
+        # high level version
+        self.resource.Object(bucket, file_key).delete()
+        # low level version. needs the object to be null
+        # self.client.delete_object(Bucket=bucket, Key=file_key)
+
+    def delete_files(self, bucket, file_keys):
+        # high level version, might be inefficient
+        for key in file_keys:
+            self.delete_file(bucket, key)
+        # low level version, needs the objects to be null
+        # objects = [{'Key': key, 'VersionId': 'null'} for key in file_keys]
+        # delete_dict = {'Objects': objects, 'Quiet': True}
+        # self.client.delete_objects(Bucket=bucket, Delete=delete_dict)
+
     def write_dataframe_to_csv_file(self, dataframe, bucket, file_key, encoding='utf-8'):
         # Get pandas dataframe as CSV bytes
         csv_buffer = StringIO()
@@ -41,3 +74,12 @@ class s3tools:
         bytes_content = csv_buffer.getvalue().encode(encoding)
 
         self.write_file(bytes_content, bucket, file_key)
+
+    def generate_quicksight_manifest(self, files, save_to):
+        """
+        TO DO: generate and save a QS manifest from a list of file paths in S3
+        :param save_to:
+        :param files:
+        :return:
+        """
+        return
