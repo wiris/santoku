1a659d264a6f718aea04979ea87892f436746333
Jenkins <jenkins@wiris.com>
2020-03-26 12:18:39 +0000

    Bump version: 0.9 → 0.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="santoku",
-    version="0.9",
+    version="0.10",
     author="Didac Fernández, Daniel Martín-Albo and Henry Qiu",
     description="ETL Toolkit for handling AWS, Salesforce and many more things.",
     packages=find_packages(),
