55ecdd3ba3848b13519b00242d8553ac748cad03
HENRYQIULO <henry@wiris.com>
2020-04-24 14:57:26 +0200

    Updated boto3 version in install_requires field in setup.py.

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     author="Didac Fernández, Daniel Martín-Albo and Henry Qiu",
     description="ETL Toolkit for handling AWS, Salesforce and many more things.",
     packages=find_packages(),
-    install_requires=["boto3>=1.12.2"],
+    install_requires=["boto3==1.9.232"],
 )
