dd9be74f9b8a74c310c2a0ee3916145d956244c4
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2020-05-11 15:36:14 +0000

    Basic BQ class w/ authentication and query capabilities

--- santoku/google/__init__.py
+++ santoku/google/__init__.py
@@ -1,1 +1,6 @@
 from .bigquery_handler import BigQueryHandler
+
+"""
+This module handles all interactions with Google's cloud services. It makes use of the official
+APIs found here: https://github.com/googleapis/google-cloud-python
+"""
