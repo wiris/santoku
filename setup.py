61f4f59fdc6d6dfa17e9dabf98a95f986c0bf544
Jenkins <jenkins@wiris.com>
2020-05-13 12:52:56 +0000

    Bump version: 0.16 → 0.17

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def load_requirements(fname):
 
 setup(
     name="santoku",
-    version="0.16",
+    version="0.17",
     author="Didac Fernández, Daniel Martín-Albo and Henry Qiu",
     description="ETL Toolkit for handling AWS, Salesforce and many more things.",
     packages=find_packages(),
