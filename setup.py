86cc87497c81b78ee5e8198b6c371d37b4ea833e
Daniel Martín-Albo <dmas@wiris.com>
2020-06-16 13:34:15 +0000

    - Calver reset. - Fixing problem with sending wheel to pypi.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def load_requirements(fname):
 
 setup(
     name="santoku",
-    version="200616.2",
+    version="200616.0",
     description="Custom Python wrapper around many third party APIs, including AWS, BigQuery, Slack and Salesforce.",
     packages=find_packages(),
     install_requires=load_requirements("requirements.txt"),
